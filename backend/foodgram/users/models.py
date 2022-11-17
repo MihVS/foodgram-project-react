e054711030d8b244571159f1f6d5896689b33141
Михаил Шутов <mihvs22@yandex.ru>
2022-11-17 10:30:22 +0300

    USERS: create app users

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from users.validators import validate_username
+
+
+class User(AbstractUser):
+    ADMIN = 'admin'
+    USER = 'user'
+    USER_ROLES = (
+        (ADMIN, 'Администратор'),
+        (USER, 'Пользователь')
+    )
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=154,
+        validators=[validate_username],
+        unique=True
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+        unique=True,
+        max_length=254,
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+    last_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150,
+    )
+    role = models.CharField(max_length=30,
+                            choices=USER_ROLES,
+                            default='user')
+
+    class Meta:
+        ordering = ('username',)
+
+    @property
+    def is_admin(self):
+        return self.role == User.ADMIN or self.is_superuser
+

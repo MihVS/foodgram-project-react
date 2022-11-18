7b8844f3b91724cf5c2d57b6d7fde6491db3b2ef
Михаил Шутов <mihvs22@gmail.com>
2022-11-18 12:24:28 +0300

    RECIPES: created models

---
+++ backend/foodgram/recipes/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=256,
+        unique=True,
+        verbose_name='Название',
+    )
+    color = models.CharField(
+        max_length=16,
+        unique=True,
+        verbose_name='Цвет',
+    )
+    slug = models.SlugField(
+        unique=True,
+    )
+
+
+class Ingredients(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    measurement_unit = models.CharField(
+        max_length=10,
+        verbose_name='Единицы измерения',
+    )
+
+
+class Recipe(models):
+    tags = models.ManyToManyField(
+        Tag,
+        # related_name='recipes',
+        verbose_name='Тег',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор',
+    )
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+
